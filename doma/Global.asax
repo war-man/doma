eb4c324891d81dd21d75ac36ba6675f0e1867693
hieuntp2 <hieuntp2@gmail.com>
2016-03-25 14:13:57 +0700

    delete

--- doma/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="doma.MvcApplication" Language="C#" %>
--- doma/Scripts/my_js/admin_addproduct.js
+++
@@ -1,5 +0,0 @@
-﻿
-app.controller('admin_addproduct_controller', ['$scope', '$rootScope', '$http', '$window', 'my_function_services', 'cart_service',
-    function ($scope, $rootScope, $http, $window, my_function_services, cart_service) {
-      
-    }]);
